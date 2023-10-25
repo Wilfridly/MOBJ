dff7901d3e0e054e94a0dfe9758475f2050b406e
Wilfridly <146443276+Wilfridly@users.noreply.github.com>
2023-10-25 18:07:30 +0200

    Add files via upload
    Dossier

---
+++ src/Node.cpp
@@ -0,0 +1,44 @@
+// -*- explicit-buffer-name: "Node.cpp<M1-MOBJ/4-5>" -*-
+
+#include  <limits>
+#include  "Node.h"
+#include  "Term.h"
+#include  "Net.h"
+#include  "Instance.h"
+#include  "Cell.h"
+
+namespace Netlist {
+
+  using namespace std;
+
+
+  const size_t  Node::noid = numeric_limits<size_t>::max();
+
+
+  Node::Node ( Term* term, size_t id  )
+    : id_      (id)
+    , term_    (term)
+    , position_()
+  { }
+
+
+  Node::~Node ()
+  {
+    if (getNet()) getNet()->remove( this );
+  }
+
+
+  inline Net* Node::getNet () const { return term_->getNet(); }
+
+
+// Node::toXml() à écrire ici.
+  void  Node::toXml ( std::ostream& stream ){
+      stream << "<node term=\"" << term_->getName()  << "\" " << " id=\"" << getId();
+      stream << "\"" << " x=\"" << position_.getX() << "\""<< " y=\"" << position_.getY() << "\">\n";
+    if(term_->isInternal()==true){
+      stream << "<node term=\"" << term_->getName()  << "\" " << " instance=\"" << term_->getInstance()->getName();
+      stream << "\"" << " id=\"" << getId() << "\"" << " x=\"" << position_.getX() << "\""<< " y=\"" << position_.getY() << "\">\n";
+    }
+  }
+
+}  // Netlist namespace.
