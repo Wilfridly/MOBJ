6966af21c2f70f75c0c37655585ed781c815e1f5
Wilfridly <146443276+Wilfridly@users.noreply.github.com>
2023-11-14 20:00:01 +0100

    Add files via upload

---
+++ src6/Instance.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+
+#include "Instance.h"
+#include "Cell.h"
+#include "Term.h"
+#include "Point.h"
+#include "Net.h"
+
+namespace Netlist{
+    Instance::Instance( Cell* owner, Cell* model, const std::string& name){ //ctor
+        owner_ = owner;
+        masterCell_ = model;
+        name_ = name;
+        terms_ = std::vector<Term*>();
+        position_ = Point(0,0);
+        owner_->add(this);
+        // std::cout << "Debug::Instance CTOR" << std::endl;
+
+        std::vector<Term*> terms = model->getTerms(); //création d'un term et emplacement dans terms
+        for ( std::vector<Term*>::const_iterator it = terms.begin() ; it != terms.end() ; ++it ) {
+            new Term(this, *it);
+        }
+    }
+    
+    Instance::~Instance(){ //dtor
+        owner_->remove(this);
+        while (not terms_.empty()) delete *terms_.begin();
+    }
+
+    //Accessseurs
+    const std::string& Instance::getName()          const{return name_;}
+    Cell* Instance::getMasterCell       ()          const{return masterCell_;}
+    Cell* Instance::getCell             ()          const{return owner_;}
+    const std::vector<Term*>& Instance::getTerms()  const{return terms_;}
+
+
+    Term* Instance::getTerm( const std::string& name) const{
+        for ( std::vector<Term*>::const_iterator it = terms_.begin() ; it != terms_.end() ; ++it ) {
+            if ((*it)->getName() == name)  return *it;
+        }
+        return NULL;
+    }
+
+    Point Instance::getPosition() const{return position_;}
+
+    //Modificateurs
+    bool  Instance::connect       ( const std::string& name, Net* net){
+        Term* term = getTerm(name);
+        if (term == NULL) return false;
+        term->setNet(net);
+        return true;
+    }
+
+    void  Instance::add           ( Term* term){
+        if(getTerm(term->getName())!= NULL) return;
+        terms_.push_back(term);
+    }
+
+    void  Instance::remove        ( Term* term){
+        for ( std::vector<Term*>::iterator it = terms_.begin() ; it != terms_.end() ; ++it ) {
+            if (*it == term) terms_.erase( it );
+        }
+    }
+
+    void  Instance::setPosition   ( const Point& pos){
+        position_ = pos;
+    }
+
+    void  Instance::setPosition   ( int x, int y ){
+        position_ = Point(x,y);
+    }
+
+    void  Instance::toXml ( std::ostream& stream ){
+        stream << indent << "<instance name=\"" << name_ << "\" mastercell=\"" << masterCell_->getName() << "\"" <<" x=\"" << position_.getX() << "\" y=\"" << position_.getY() << "\"/>\n";
+    }
+}
---
+++ src6/Instance.h
@@ -0,0 +1,43 @@
+#ifndef NETLIST_INSTANCE_H
+#define NETLIST_INSTANCE_H
+
+#include <string>
+#include <vector>
+
+#include "Cell.h"
+#include "Term.h"
+#include "Point.h"
+#include "Net.h"
+
+namespace Netlist{
+    class Point;
+
+    class Instance{
+        private :
+            Cell*               owner_;
+            Cell*               masterCell_;
+            std::string         name_;
+            std::vector<Term*>  terms_;
+            Point               position_;
+        public :
+            //Constructeurs & Destructeur
+            Instance      ( Cell* owner, Cell* model, const std::string& );
+            ~Instance      ();
+
+            //Accessseurs
+            const std::string&        getName       () const;
+            Cell*                     getMasterCell () const;
+            Cell*                     getCell       () const;
+            const std::vector<Term*>& getTerms      () const;
+            Term*                     getTerm       ( const std::string& ) const;
+            Point                     getPosition   () const;
+            void                      toXml         ( std::ostream& stream );
+            //Modificateurs
+            bool  connect       ( const std::string& name, Net* );
+            void  add           ( Term* );
+            void  remove        ( Term* );
+            void  setPosition   ( const Point& );
+            void  setPosition   ( int x, int y );
+    };
+}
+#endif
